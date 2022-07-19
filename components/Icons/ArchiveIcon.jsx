7cdc6ec9e4bb8c58f5db18df71d03cf00c3c6e4b
VahanMuradyan <10vahan.m16@gmail.com>
2022-07-19 15:51:35 +0400

    fixes for build

--- components/Icons/ArchiveIcon.jsx
+++ components/Icons/ArchiveIcon.jsx
@@ -14,8 +14,8 @@ const ArchiveIcon = () => {
         fill="black"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M3 8H17V15C17 16.1046 16.1046 17 15 17H5C3.89543 17 3 16.1046 3 15V8ZM8 11C8 10.4477 8.44772 10 9 10H11C11.5523 10 12 10.4477 12 11C12 11.5523 11.5523 12 11 12H9C8.44772 12 8 11.5523 8 11Z"
         fill="black"
       />
